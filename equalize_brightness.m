a34268c56f738606f37d3b158239cd69c88fc9b6
angelawh <angelah@princeton.edu>
2017-01-06 00:52:30 -0600

    Code to equalize brightness

---
+++ equalize_brightness.m
@@ -0,0 +1,21 @@
+%
+% equalize_brightness.m
+%   Equalizes brightness across all images in folder images/name.
+%
+
+function equalize_brightness(name)
+    cd(fullfile('./images/', name));
+    curDir = pwd();
+    
+    im_orig = imread(fullfile(curDir, '001.jpg'));
+    
+    files = dir(curDir);
+    num = size(files);
+    num = num(1) - 2;
+    
+    for i = 2:num
+        fullname = fullfile(curDir, [sprintf('%03d',i) '.jpg']);
+        im_new = imread(fullname);
+        output = imhistmatch(im_new, im_orig);
+        imwrite(output, fullname);
+    end
