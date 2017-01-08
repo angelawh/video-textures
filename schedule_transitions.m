fd1a6dfeace4ffab13542ffea8f98d13fe72a99e
angelawh <angelah@princeton.edu>
2017-01-08 01:14:39 -0600

    Function set up for scheduling transitions

---
+++ schedule_transitions.m
@@ -0,0 +1,9 @@
+%
+% schedule_transitions.m
+%   Schedules order of transitions to be used.
+%   Takes in a set of all transitions to be used (even if multiples)
+%   Returns a list of all transitions i -> j in order.
+
+function [ordered_transitions] = schedule_transitions(transitions)
+
+end
