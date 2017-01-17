b8200c5be379b87df2c2d6f3fad61743b1064f3b
angelawh <angelah@princeton.edu>
2017-01-17 14:17:00 -0500

    have it fully run it all and make 6 videos automatically

--- run_it_all.m
+++ run_it_all.m
@@ -1,20 +1,17 @@
-%run_it_all.m
+% run_it_all.m
+% Creates crossfaded and non-faded versions using all 3 distance metrics
 
-function run_it_all(name, video_name, resize, m_weight, local_size, ...
-                    cost_thresh, num_transitions, length, fade_l)
+function run_it_all(name, video_name, resize, start, finish, m_weight, ...
+                        local_size, num_transitions, length, fade_l)
 
-    
+    [images,resized_images] = read_images(name, start, finish, resize);
 
-    [images,resized_images] = read_images(name, NaN, NaN, resize);
-
-    D = hsv_l2_distance(resized_images);
-    %or
-    %D = l2_distance(ims);
-    %or
-    %D = im_euclid_distance(ims);
+    %% Create l2 distance black/white videos
+    D = l2_distance(images);
     
     D_prime = prime_distance(D, m_weight, NaN, NaN, NaN);
     
+    cost_thresh = .1; % For L2 distance
     primitive_loops = prune(D_prime, D_prime, local_size, cost_thresh, ...
                                num_transitions);
                            
@@ -22,9 +19,49 @@ function run_it_all(name, video_name, resize, m_weight, local_size, ...
     
     ordered_transitions = schedule_transitions(transitions);
     
-    synthesize_video(resized_images, ordered_transitions, name, ...
-                                video_name, false, fade_l);
-
+    synthesize_video(images, ordered_transitions, name, ...
+                     [video_name, '_bwl2_nofade'], false, fade_l);
+                            
+    synthesize_video(images, ordered_transitions, name, ...
+                     [video_name, '_bwl2_fade'], true, fade_l);
+    
+    %% Create hsv hist distance
+    D_hist = hsv_l2_distance(resized_images);
+    
+    D_prime = prime_distance(D_hist, m_weight, NaN, NaN, NaN);
+    
+    cost_thresh = 100; % For hsv distance
+    primitive_loops = prune(D_prime, D_prime, local_size, cost_thresh, ...
+                               num_transitions);
+                           
+    transitions = select_transitions(primitive_loops, length);
+    
+    ordered_transitions = schedule_transitions(transitions);
+    
+    synthesize_video(images, ordered_transitions, name, ...
+                     [video_name, '_hsvdist_nofade'], false, fade_l);
+                            
+    synthesize_video(images, ordered_transitions, name, ...
+                     [video_name, '_hsvdist_fade'], true, fade_l);
+                 
+    %% Create Euclid dist videos
+    D_euclid = im_euclid_distance(resized_images);
+    
+    D_prime = prime_distance(D_euclid, m_weight, NaN, NaN, NaN);
+    
+    cost_thresh = 1; % For euclid distance
+    primitive_loops = prune(D_prime, D_prime, local_size, cost_thresh, ...
+                               num_transitions);
+                           
+    transitions = select_transitions(primitive_loops, length);
+    
+    ordered_transitions = schedule_transitions(transitions);
+    
+    synthesize_video(images, ordered_transitions, name, ...
+                     [video_name, '_iedist_nofade'], false, fade_l);
+                            
+    synthesize_video(images, ordered_transitions, name, ...
+                     [video_name, '_iedist_fade'], true, fade_l);
 
 
 end
